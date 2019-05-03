c7d0808e01746f72be03901e83faf96138e70d42
Joachim Holwech <joachim.holwech@gmail.com>
2019-05-03 21:35:43 +0200

    Now opening and view content is working

--- src/ZipEditor.ts
+++ src/ZipEditor.ts
@@ -1,14 +1,50 @@
-// import * as zip from 'jszip';
-const JSZip = require('jszip');
+// const JSZip = require('jszip');
+import JSZip, { JSZipObject, OutputType } from 'jszip';
+
+interface HTMLInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
+
+interface File {
+  name: string,
+  relativePath: string,
+  content: any,
+  outputType: OutputType,
+}
 
 export default class ZipEditor {
   private zip = new JSZip();
-  public print(): void {
-    console.log('hi');
+
+  public async openFile(event: Event): Promise<void> {
+    console.log(this.zip)
+    const zipFile = (event as HTMLInputEvent).target.files![0]
+    const reader = new FileReader();
+    const decoded = await this.zip.loadAsync(zipFile);
+    const fileList = decoded.forEach(async (relativePath, zipEntry) => {
+      console.log(await this.decodeEntry(relativePath, zipEntry, 'text'))
+    });
+      // zip.loadAsync(file)
+      // .then(function(zipFolder) {
+      //     return zipFolder.forEach((relativePath, zipEntry) => {
+      //         console.log(zipEntry.name);
+      //         zipEntry.async('string').then((text) => {
+      //             let newContent = text + ' YO!';
+      //             zip.file(zipEntry.name, newContent);
+      //         });
+      //     });
+      //     // zip_folder.file("test 1.txt").async("binary").then((text) => console.log(text)); // a promise of "Hello World\n"
+      // }).then(() => {
+      //     console.log('foreach done')
+      // });
   }
 
-  public test(): void {
-    console.log(this.zip);
-    this.zip.file('hello.txt', 'Hello[p my)6cxsw2q');
+  public async decodeEntry(relativePath: string, zipEntry: JSZipObject, outputType: OutputType): Promise<File> {
+    const content = await zipEntry.async(outputType);
+    return {
+      name: zipEntry.name,
+      relativePath,
+      content,
+      outputType
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import ZipEditor from './ZipEditor';
 
 window.onload = () => {
   const editor = new ZipEditor();
-  document.getElementById('file')!.addEventListener('change', editor.test, false);
+  document.getElementById('file')!.addEventListener('change', editor.openFile.bind(editor), false);
 }
