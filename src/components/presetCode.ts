2eececa83fec30ec05f679eda3dec15869abb1bd
Joachim Holwech <joachim.holwech@gmail.com>
2019-11-14 19:46:19 +0100

    Added some test files. Created example that can fix WhatsApp backups

--- src/components/presetCode.ts
+++ src/components/presetCode.ts
@@ -1,35 +1,37 @@
 export const presetCode = [
   {
-    text: 'Example',
-    value: `
-{
+    text: 'Fix WhatsApp image backup',
+    value: `{
   settings: {
-    re: /(.jpg|.png|.gif|.ps|.jpeg)$/,
+    re: /(.jpg|.png|.gif|.jpeg)$/,
+    pad: function pad(n, width, z) {
+      z = z || '0';
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
   },
   process: (relativePath, entry, content, newZip, settings) => {
     if (!settings.re.test(entry.name)) {
       return;
     }
     const exif = load(content);
+    let dateStr = relativePath.split('-')[1];
+    let year = parseInt(dateStr.substring(0, 4));
+    let month = parseInt(dateStr.substring(4, 6));
+    let day = parseInt(dateStr.substring(6));
     if (exif.Exif) {
-      exif.Exif[TagValues.ExifIFD.DateTimeOriginal] = '2010:10:10 10:10:10';
+      exif.Exif[TagValues.ExifIFD.DateTimeOriginal] = year + ":" + settings.pad(month, 2) + ":" + settings.pad(day, 2) + " 00:00:00";
     } else {
       exif.Exif = {
-        [TagValues.ExifIFD.DateTimeOriginal]: '2010:10:10 10:10:10',
+        [TagValues.ExifIFD.DateTimeOriginal]: year + ":" + settings.pad(month, 2) + ":" + settings.pad(day, 2) + " 00:00:00",
       };
     }
     const exifBytes = dump(exif);
     content = insert(exifBytes, content);
     const name = entry.name.split('.');
-    newZip.file(name[0] + '_modified.' + name[1], content, { binary: true });
+    newZip.file(name[0] + '.' + name[1], content, { binary: true });
   }
 }
 `
   },
-  {
-    text: 'WhatsApp backup fixer',
-    value: `
-    lol
-`
-  }
 ]
